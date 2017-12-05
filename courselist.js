2c521c5d4cd82755083d12c999875f53f4ce2910
APinCode <alvaropinedo22@yahoo.es>
2017-12-05 13:14:22 +0100

    Color COnflicts

--- courselist.js
+++ courselist.js
@@ -6,10 +6,20 @@ function buildMap(obj) {
     return map;
 }
 
+function isIterable(obj) {
+  // checks for null and undefined
+  if (obj == null) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+}
+
 class CourseList {
 
 	constructor() {
 		this.enableCourseList = [];
+    this.coursesInList = [];
+    this.conflictList = new Map();
 	}
 
 	createCourseList(data) {
@@ -31,12 +41,9 @@ class CourseList {
 
 			let courseName = document.createElement("td");
 			courseName.innerHTML = course;
+      courseName.id = course + "_button";
+      courseName.style.background =  "#f6f6f6"
 
-			let button = document.createElement("button");
-			button.innerHTML = "click";
-			button.id = course+"_button";
-
-			row.appendChild(button);
 			row.appendChild(courseName)
 			tbody.appendChild(row);
 		}
@@ -44,17 +51,91 @@ class CourseList {
 		table.appendChild(tbody);
 	}
 
-	enableCourse(metadata) {
-		if(this.enableCourseList.includes(metadata)) {
-			var index = this.enableCourseList.indexOf(metadata);
+	enableCourse(c) {
+		if(this.coursesInList.includes(c)) {
+			var index = this.coursesInList.indexOf(c);
 			if (index > -1) {
     		this.enableCourseList.splice(index, 1);
-			}
+        this.coursesInList.splice(index, 1);
+        // timtable.removeCourse(c);
+        var wasOrange = false;
+        for(let conflict of this.conflictList.get(c)) {
+          if(document.getElementById(conflict+"_button").style.background === "orange"||document.getElementById(conflict+"_button").style.background === "green") {
+            document.getElementById(conflict+"_button").style.background = "green";
+            wasOrange = true;
+          } else {
+            document.getElementById(conflict+"_button").style.background = "#f6f6f6"
+          }
+        }
+        if(wasOrange) {
+          document.getElementById(c+"_button").style.background = "red";
+        } else {
+          document.getElementById(c+"_button").style.background = "#f6f6f6"
+        }
+      }
 		} else {
-				this.enableCourseList.push(metadata)
+				this.enableCourseList.push(c)
+        this.coursesInList.push(c)
+        // timtable.addCourse(c);
+        var wasGreen = false;
+        for(let conflict of this.conflictList.get(c)) {
+          if(document.getElementById(conflict+"_button").style.background === "green" || document.getElementById(conflict+"_button").style.background === "orange") {
+            document.getElementById(conflict+"_button").style.background = "orange";
+            wasGreen = true;
+          } else {
+            document.getElementById(conflict+"_button").style.background = "red"
+          }
+        }
+        if(wasGreen) {
+          document.getElementById(c+"_button").style.background = "orange";
+        } else {
+          document.getElementById(c+"_button").style.background = "green";
+        }
 		}
+    for(let conf of this.enableCourseList) {
+      var goesOrange = false;
+      if(isIterable(conf)) {
+        // console.log(Array.from(this.conflictList.get(conf)))
+        for(let list of this.conflictList.get(conf)) {
+          console.log(list);
+          if(document.getElementById(list+"_button").style.background === "green" || document.getElementById(list+"_button").style.background === "orange") {
+            document.getElementById(list+"_button").style.background = "orange";
+            goesOrange = true;
+          } else {
+            document.getElementById(list+"_button").style.background = "red"
+          }
+        }
+        if(goesOrange) {
+          document.getElementById(conf+"_button").style.background = "orange"
+        } else {
+          document.getElementById(conf+"_button").style.background = "green"
+        }
+      }
+    }
 		timtable.createTimetable(this.enableCourseList)
 	}
+
+// remove and do a function that precomputes the conflicts for each course
+  conflicts(data) {
+    for(let [course, metadata] of data) {
+      var list = [];
+      for(let [otherCourse, otherMetadata] of data) {
+        if(course !== otherCourse) {
+          for(let timeslot of metadata.timeslots) {
+            for(let otherTimeslot of otherMetadata.timeslots) {
+              if(timeslot.day === otherTimeslot.day && timeslot.time === otherTimeslot.time) {
+                list.push(otherCourse);
+              }
+            }
+          }
+        }
+      }
+      var uniqueList = list.filter(function(item, pos, self) {
+          return self.indexOf(item) == pos;
+      })
+      this.conflictList.set(course, uniqueList);
+    }
+  }
 
 }
 
@@ -62,8 +143,9 @@ class CourseList {
 let courselist = new CourseList()
 let data_map = buildMap(ISA_data);
 courselist.createCourseList(data_map)
+courselist.conflicts(data_map)
 for(let [course, metadata] of data_map) {
-	document.getElementById(course+"_button").onclick = () => courselist.enableCourse([course, metadata]);
+	document.getElementById(course+"_button").onclick = () => courselist.enableCourse(course);
 }
 //var courseList = new courseList();
 //courseList.createTimetable(ISA_data);
